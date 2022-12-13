516f185e3c6df47f67935be448d806d1e90dd7dd
Juan <juanpablo.munoz@uber.com>
2022-12-12 22:54:09 -0500

    create posts and save in DB

---
+++ handlers/post.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/juanmunoz22-bit/go-rest-ws/models"
+	"github.com/juanmunoz22-bit/go-rest-ws/repository"
+	"github.com/juanmunoz22-bit/go-rest-ws/server"
+	"github.com/juanmunoz22-bit/go-rest-ws/utils"
+	"github.com/segmentio/ksuid"
+)
+
+type InsertPostRequest struct {
+	PostContent string `json:"post_content"`
+}
+
+type PostResponse struct {
+	Id          string `json:"id"`
+	PostContent string `json:"post_content"`
+}
+
+func InsertPostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := utils.StringToken(r)
+		token, err := utils.ClaimTokenFromAuth(s, tokenString, models.AppClaims{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			var postRequest = InsertPostRequest{}
+			if err := json.NewDecoder(r.Body).Decode(&postRequest); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			id, err := ksuid.NewRandom()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			post := models.Post{
+				Id:          id.String(),
+				PostContent: postRequest.PostContent,
+				UserId:      claims.UserId,
+			}
+			err = repository.InsertPost(r.Context(), &post)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(PostResponse{
+				Id:          post.Id,
+				PostContent: post.PostContent,
+			})
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
