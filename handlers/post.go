79ef25f3503c19472eae52e168afe41967716d10
Juan <juanpablo.munoz@uber.com>
2023-01-04 20:29:53 -0500

    crud for posts completed

--- handlers/post.go
+++ handlers/post.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/juanmunoz22-bit/go-rest-ws/models"
 	"github.com/juanmunoz22-bit/go-rest-ws/repository"
 	"github.com/juanmunoz22-bit/go-rest-ws/server"
@@ -11,13 +13,17 @@ import (
 	"github.com/segmentio/ksuid"
 )
 
-type InsertPostRequest struct {
+type UpsertPostRequest struct {
 	PostContent string `json:"post_content"`
 }
 
 type PostResponse struct {
 	Id          string `json:"id"`
 	PostContent string `json:"post_content"`
+}
+
+type PostUpdateResponse struct {
+	Message string `json:"message"`
 }
 
 func InsertPostHandler(s server.Server) http.HandlerFunc {
@@ -29,7 +35,7 @@ func InsertPostHandler(s server.Server) http.HandlerFunc {
 			return
 		}
 		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
-			var postRequest = InsertPostRequest{}
+			var postRequest = UpsertPostRequest{}
 			if err := json.NewDecoder(r.Body).Decode(&postRequest); err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
@@ -58,5 +64,102 @@ func InsertPostHandler(s server.Server) http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+	}
+}
+
+func GetPostByIdHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		post, err := repository.GetPostById(r.Context(), params["id"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(post)
+	}
+}
+
+func UpdatePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		tokenString := utils.StringToken(r)
+		token, err := utils.ClaimTokenFromAuth(s, tokenString, models.AppClaims{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			var postRequest = UpsertPostRequest{}
+			if err := json.NewDecoder(r.Body).Decode(&postRequest); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			post := models.Post{
+				Id:          params["id"],
+				PostContent: postRequest.PostContent,
+				UserId:      claims.UserId,
+			}
+			err = repository.UpdatePost(r.Context(), &post)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(PostUpdateResponse{
+				Message: "Post updated successfully",
+			})
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func DeletePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		tokenString := utils.StringToken(r)
+		token, err := utils.ClaimTokenFromAuth(s, tokenString, models.AppClaims{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			err = repository.DeletePost(r.Context(), params["id"], claims.UserId)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(PostUpdateResponse{
+				Message: "Post deleted successfully",
+			})
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func ListPostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		pageString := r.URL.Query().Get("page")
+		var page = uint64(0)
+		if pageString != "" {
+			page, err = strconv.ParseUint(pageString, 10, 64)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+		}
+		posts, err := repository.ListPost(r.Context(), page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(posts)
 	}
 }
--- repository/repository.go
+++ repository/repository.go
@@ -11,6 +11,10 @@ type Repository interface {
 	GetUserByID(ctx context.Context, id string) (*models.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
 	InsertPost(ctx context.Context, post *models.Post) error
+	GetPostById(ctx context.Context, id string) (*models.Post, error)
+	UpdatePost(ctx context.Context, post *models.Post) error
+	DeletePost(ctx context.Context, id string, userId string) error
+	ListPost(ctx context.Context, page uint64) ([]*models.Post, error)
 	Close() error
 }
 
@@ -34,6 +38,22 @@ func GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
 
 func InsertPost(ctx context.Context, post *models.Post) error {
 	return implementation.InsertPost(ctx, post)
+}
+
+func GetPostById(ctx context.Context, id string) (*models.Post, error) {
+	return implementation.GetPostById(ctx, id)
+}
+
+func UpdatePost(ctx context.Context, post *models.Post) error {
+	return implementation.UpdatePost(ctx, post)
+}
+
+func DeletePost(ctx context.Context, id string, userId string) error {
+	return implementation.DeletePost(ctx, id, userId)
+}
+
+func ListPost(ctx context.Context, page uint64) ([]*models.Post, error) {
+	return implementation.ListPost(ctx, page)
 }
 
 func Close() error {
